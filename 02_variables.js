bb91bc7bc0e6ecaded215f10936052b9e237da2a
Chetan Yadav <ydvchetan01@gmail.com>
2024-01-20 13:43:41 +0530

    modified

--- 02_variables.js
+++ 02_variables.js
@@ -1,18 +1,15 @@
 const accountId = 144553
 let accountEmail = "chetan@gmail.com"
 var accountPassword = "12345"
-accountCity = "Indore"
+accountCity = "Indore" // Not recom
 let accountState;
 
 // accountId = 2 // not allowed
+console.log(accountId);
 
 accountEmail = "user@gmail.com"
-
 accountPassword = "212111"
-
 accountCity = "Sanawad"
-
-console.log(accountId);
 
 /*
 Prefer not to use var
