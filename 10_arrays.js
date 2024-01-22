965d99118cb76ef68a7352c652550e58047e86d8
Chetan Yadav <ydvchetan01@gmail.com>
2024-01-22 08:23:59 +0530

    Arrays Basic

---
+++ 10_arrays.js
@@ -0,0 +1,47 @@
+const myArr = [0, 1, 2, 3, 4, 5]
+console.log(myArr[0]);
+
+const myArrOne = [0, 1, 2, 3, 4, 5, true, "chetan"]
+
+const myHeros = ["Jetha", "Iyer"]
+
+const myArr2 = new Array(1, 2, 3, 4)
+
+// Array methods
+
+myArr.push(6)
+console.log(myArr);
+
+myArr.push(7)
+console.log(myArr);
+
+myArr.pop()
+console.log(myArr);
+
+myArr.unshift(9)
+console.log(myArr);
+
+myArr.shift()
+console.log(myArr);
+
+console.log(myArr.includes(9));
+console.log(myArr.indexOf(9));
+console.log(myArr.indexOf(3));
+
+const newArr = myArr.join()
+console.log(myArr);
+console.log(typeof newArr);
+console.log(newArr);
+
+
+// slice, splice
+
+console.log("A ", myArr);
+
+const myn1 = myArr.slice(1, 3)
+console.log(myn1);
+console.log("B", myArr);
+
+const myn2 = myArr.splice(1, 3)
+console.log("C", myArr);
+console.log(myn2);
