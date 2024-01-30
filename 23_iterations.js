13cd80d010953524959797881709a047c23dc89d
Chetan Yadav <ydvchetan01@gmail.com>
2024-01-30 18:39:25 +0530

    Iterations

---
+++ 23_iterations.js
@@ -0,0 +1,82 @@
+// for loop
+
+for (let i = 0; i <= 10; i++) {
+    const element = i;
+    if (element == 5) {
+        console.log("5 is best number");
+    }
+    console.log(element);
+}
+
+// console.log(element); error --> element is not defined
+
+
+for (let i = 0; i <= 10 ; i++) {
+    console.log(`Outer loop value: ${i}`);
+    for (let j = 0; j <= 10; j++) {
+        console.log(`Inner loop value: ${j} and inner value ${i}`);
+    }
+}
+
+
+for (let i = 1; i <= 10 ; i++) {
+    console.log(`Table: ${i}`);
+    for (let j = 1; j <= 10; j++) {
+        console.log(i + '*' + j + ' = ' + i*j);
+    }
+}
+
+
+let myArray = ["Jethalal", "Babitaji", "Iyer"]
+console.log(myArray.length);
+for (let index = 0; index < myArray.length; index++) {
+    const element = myArray[index];
+    console.log(element);
+}
+
+// <= --> undefined
+
+
+// break and continue
+
+for (let index = 1; index <= 20; index++) {
+    if (index == 5) {
+        console.log(`Detected 5`);
+        break
+    }
+    console.log(`Value of index is ${index}`);
+}
+
+
+for (let index = 1; index <= 20; index++) {
+    if (index == 5) {
+        console.log(`Detected 5`);
+        continue
+    }
+    console.log(`Value of index is ${index}`);
+}
+
+
+// while and do-while loop
+
+let index = 0
+while (index <= 10) {
+    console.log(`Value of index is ${index}`);
+    index = index + 2
+}
+
+
+let myArrayOne = ['Tarak', "Bhide", "Popat"]
+let arr = 0
+while (arr < myArrayOne.length) {
+    console.log(`Member is ${myArray[arr]}`);
+    arr = arr + 1
+}
+
+
+let score = 11
+
+do {
+    console.log(`Score is ${score}`);
+    score++
+} while (score <= 10);
