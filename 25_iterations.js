e024afafb386e713d8e567751ab92ee5959d33a4
Chetan Yadav <ydvchetan01@gmail.com>
2024-02-01 10:40:12 +0530

    Iterations part three

---
+++ 25_iterations.js
@@ -0,0 +1,110 @@
+const coding = ["js", "ruby", "java", "python", "cpp"]
+
+const values = coding.forEach( (item) => {
+    console.log(item);
+    return item
+} )
+
+console.log(values);
+
+
+const myNumsOne = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+// const newNums = myNumsOne.filter((num) => num > 4)
+
+const newNums = myNumsOne.filter((num) => {
+    return num > 4
+})
+
+console.log(newNums);
+
+
+const newNumsOne = []
+
+myNumsOne.forEach( (num) => {
+    if (num > 4) {
+        newNumsOne.push(num)
+    }
+})
+
+console.log(newNumsOne);
+
+
+const books = [
+    { title: 'Book One', genre: 'Fiction', publish: 1981, edition: 2004 },
+    { title: 'Book Two', genre: 'Non-Fiction', publish: 1992, edition: 2008 },
+    { title: 'Book Three', genre: 'History', publish: 1999, edition: 2007 },
+    { title: 'Book Four', genre: 'Non-Fiction', publish: 1989, edition: 2010 },
+    { title: 'Book Five', genre: 'Science', publish: 2009, edition: 2014 },
+    { title: 'Book Six', genre: 'Fiction', publish: 1987, edition: 2010 },
+    { title: 'Book Seven', genre: 'History', publish: 1986, edition: 1996 },
+    { title: 'Book Eight', genre: 'Science', publish: 2011, edition: 2016 },
+    { title: 'Book Nine', genre: 'Non-Fiction', publish: 1981, edition: 1989 },
+];
+
+let userBooks = books.filter((bk) => bk.genre === 'History')
+console.log(userBooks);
+
+userBooks = books.filter( (bk) => bk.publish >= 1995 )
+console.log(userBooks);
+
+userBooks = books.filter( (bk) => {return bk.publish >= 1995} )
+console.log(userBooks);
+
+userBooks = books.filter( (bk) => {
+    return bk.publish >= 1995 && bk.genre === 'History'
+} )
+console.log(userBooks);
+
+
+const myNumbers = [1, 2, 3, 4, 5, 5, 6, 7, 8, 9, 10]
+
+const newNumsStar = myNumbers.map( (num) => num + 10 )
+console.log(newNumsStar);
+
+// const newNumsStar = myNumbers.map( (num) => {return num + 10} )
+// console.log(newNumsStar);
+
+const newNumsOneStar = myNumbers
+                        .map( (num) => num * 10 )
+                        .map( (num) => num + 1 )
+                        .filter( (num) => num >= 40)
+
+console.log(newNumsOneStar);
+
+
+const myNums = [1, 2, 3]
+
+const myTotal = myNums.reduce(function (acc, currval) {
+    console.log(`acc: ${acc} and currval: ${currval}`);
+    return acc + currval
+}, 0)
+
+console.log(myTotal);
+
+const myTotalOne = myNums.reduce( (acc, curr) => acc + curr, 0)
+console.log(myTotalOne);
+
+
+const shoppingCart = [
+    {
+        itemName: "js course",
+        price: 2999
+    },
+    {
+        itemName: "py course",
+        price: 999
+    },
+    {
+        itemName: "mobile dev course",
+        price: 5999
+    },
+    {
+        itemName: "data science course",
+        price: 12999
+    }
+]
+
+const priceToPay = shoppingCart.reduce((acc, item) => acc + item.price, 0)
+
+console.log(priceToPay);
