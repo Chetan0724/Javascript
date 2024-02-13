1a16785279d9135dc6643ae03cb7e84518b4731e
Chetan Yadav <ydvchetan01@gmail.com>
2024-02-13 23:40:15 +0530

    Object Memory Concept

---
+++ JS Exploration/01_script.js
@@ -0,0 +1,14 @@
+let userOne = {
+    collegeId: 1054,
+    collegeBranch: "CSE"
+}
+
+let userTwo = userOne
+
+userTwo = {
+    collegeId: 1060,
+    collegeBranch: "FT"
+}
+
+console.log(userOne);
+console.log(userTwo);
