1ece85017b3f127ff028ac006490545ba7fdb03c
Chetan Yadav <ydvchetan01@gmail.com>
2024-03-15 12:11:44 +0530

    Modify file

--- 23_iterations.js
+++ 23_iterations.js
@@ -11,7 +11,7 @@ for (let i = 0; i <= 10; i++) {
 // console.log(element); error --> element is not defined
 
 
-for (let i = 0; i <= 10 ; i++) {
+for (let i = 0; i <= 10; i++) {
     console.log(`Outer loop value: ${i}`);
     for (let j = 0; j <= 10; j++) {
         console.log(`Inner loop value: ${j} and inner value ${i}`);
@@ -23,6 +23,7 @@ for (let i = 1; i <= 10 ; i++) {
     console.log(`Table: ${i}`);
     for (let j = 1; j <= 10; j++) {
         console.log(i + '*' + j + ' = ' + i*j);
+        // console.log(`${i}*${j} = ${i*j}`);
     }
 }
 
