a9ee283602a2cbb0a3a8bdd71d4bfe2007181a21
Chetan Yadav <ydvchetan01@gmail.com>
2024-04-28 16:56:04 +0530

    restart

---
+++ JS Concepts/01_script.js
@@ -0,0 +1,16 @@
+// RAM memory available on the device
+const memory = navigator.deviceMemory;
+console.log(`${memory} GB`);
+
+
+let arr = [1, 2, 13, 24, 52]
+arr.length = 2;
+console.log(arr);
+
+
+console.log(1 + "1" - 1);
+
+
+console.log(0.1 + 0.2 == 0.3)
+console.log(0.1+ 0.2);
+console.log(0.3);
